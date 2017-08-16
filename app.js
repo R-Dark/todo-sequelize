c78ba03268e459760bcb6d26eeb5b944d3558f1d
rob dark <robdark@robs-MacBook-Pro.local>
2017-08-16 14:24:01 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const app = express()
+const mustache = require("mustache-express")
+const bodyParser = require("body-parser")
+app.engine('mustache', mustache())
+app.set('view engine', 'mustache')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+
+const todos = [
+  "Daily Project"
+];
+
+const completed = [
+  "Learn Node"
+];
+
+app.get("/", function(require, response) {
+  response.render('index', {
+    todos: todos,
+    completed: completed
+  });
+});
+
+app.post("/", function(require, response) {
+  todos.push(require.body.todos);
+  response.redirect('/');
+})
+
+
+
+app.listen(3000, function() {
+  console.log("Express started on port 3000")
+})
